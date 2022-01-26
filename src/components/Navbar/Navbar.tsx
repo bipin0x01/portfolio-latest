18a3c5fa00565714481174bf26179ff65a7126a9
Bipin Thapa <bt.kaji@gmail.com>
2022-01-26 18:59:09 +0545

    Created NavItem Array

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,42 +1,40 @@
 import Image from "next/image";
 import { useState } from "react";
 import { Navbar, Nav, Row, Container } from "react-bootstrap";
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 export default function MainNavbar() {
+  const NavItems = [
+    { link: "#about", name: "About Me" },
+    { link: "#skills", name: "Skills" },
+    { link: "#projects", name: "Projects" },
+    { link: "#blogs", name: "Blogs" },
+    { link: "#contact", name: "Contact" },
+  ];
 
-    // const [horizontal, sethorizontal] = useState(false);
-
-    // const changeNavbarLayout = () =>{
-    //     if(window.scrollY >= 640){
-    //         sethorizontal(true);
-    //     }
-    //     else{
-    //       sethorizontal(false);
-    //     }
-    // }
-
-    //     window.addEventListener('scroll', changeNavbarLayout);   
-
-    return (
-        <> 
-           <Row className={'justify-content-center'}> {/*&& horizontal?`${styles.horizontal}`:`${styles.vertical}`*/}
-           <Navbar className={styles.navbar} bg="dark" variant="light">
-                    <Container>
-                    <Navbar.Brand className={styles.navbrand} href="#home">
-                        <Image src="/static/favicon.ico" alt="Logo" height="40" width="40"/>
-                    </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={styles.navLink}>Home</Nav.Link>
-                        <Nav.Link href="#about" className={styles.navLink}>About Me</Nav.Link>
-                        <Nav.Link href="#skills"  className={styles.navLink}>Skill</Nav.Link>
-                        <Nav.Link href="#projects"  className={styles.navLink}>Projects</Nav.Link>
-                        <Nav.Link href="#blog" className={styles.navLink}>Blogs</Nav.Link>
-                        <Nav.Link href="#contact"  className={styles.navLink}>Contact</Nav.Link>
-                    </Nav>
-                    </Container>
-            </Navbar>
-           </Row>
-        </>
-    );
+  return (
+    <>
+      <Row className={"justify-content-center"}>
+        <Navbar className={styles.navbar} bg="dark" variant="light">
+          <Container>
+            <Navbar.Brand className={styles.navbrand} href="#home">
+              <Image
+                src="/static/favicon.ico"
+                alt="Logo"
+                height="30"
+                width="30"
+              />
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              {NavItems.map((item) => (
+                <Nav.Link href={item.link} className={styles.navLink}>
+                  {item.name}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Container>
+        </Navbar>
+      </Row>
+    </>
+  );
 }
