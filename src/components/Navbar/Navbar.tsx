da71f0bc9934ab206203c940157712a07fee182b
Bipin Thapa <bt.kaji@gmail.com>
2022-01-27 01:39:49 +0545

    mobile responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export default function MainNavbar() {
   return (
     <>
       <Row className={"justify-content-center"}>
-        <Navbar className={styles.navbar} bg="dark" variant="light">
+        <Navbar expand="sm" className={styles.navbar} bg="dark" variant="light">
           <Container>
             <Navbar.Brand className={styles.navbrand} href="#home">
               <Image
@@ -25,17 +25,20 @@ export default function MainNavbar() {
                 width="30"
               />
             </Navbar.Brand>
-            <Nav className="me-auto">
-              {NavItems.map((item, index) => (
-                <Nav.Link
-                  key={index}
-                  href={item.link}
-                  className={styles.navLink}
-                >
-                  {item.name}
-                </Nav.Link>
-              ))}
-            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                {NavItems.map((item, index) => (
+                  <Nav.Link
+                    key={index}
+                    href={item.link}
+                    className={styles.navLink}
+                  >
+                    {item.name}
+                  </Nav.Link>
+                ))}
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </Row>
