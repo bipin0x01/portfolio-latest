46f9cf63c0ec1ad9964e636e05782d3200476d0a
Bipin Thapa <bt.kaji@gmail.com>
2022-01-27 02:13:52 +0545

    blogcard width fix in mobile devices

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -1,40 +1,55 @@
 import Image from "next/image";
 
-export default function BlogCard({ title, Img, description, tags, date, author }) {
-    return (
-        <>
-           <div className="col-lg-4">
-           <div className="container px-4">
-                <div className="blogcard">
-                    <div className="blogcard-header">
-                        <Image src="/static/pp.jpg" alt="" height={200} width={300} layout="responsive" />
-                    </div>
-                    <div className="blogcard-body">
-                        <div className="d-inline">
-                        {tags.map((item, i) => {
-                            return (
-                                <span key={i} className="badge badge-pill text-primary border border-primary tag mx-1">{item}</span>
-                            )
-                        })
-                        }
-                        </div>
-                        <h4>{title}</h4>
-                        <p>
-                            {description}
-                        </p>
-                        <div className="user">
-                            <div className="author-img mx-1">
-                                <Image src={Img} alt="Author" height={45} width={45} />
-                            </div>
-                            <div className="user-info">
-                                <h5>{author}</h5>
-                                <small>{date}</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+export default function BlogCard({
+  title,
+  Img,
+  description,
+  tags,
+  date,
+  author,
+}) {
+  return (
+    <>
+      <div className="col-xl-4 col-lg-4 col-md-6 my-2">
+        <div className="container px-1">
+          <div className="blogcard">
+            <div className="blogcard-header">
+              <Image
+                src="/static/pp.jpg"
+                alt=""
+                height={200}
+                width={300}
+                layout="responsive"
+              />
             </div>
-           </div>
-        </>
-    );
+            <div className="blogcard-body">
+              <div className="d-inline">
+                {tags.map((item, i) => {
+                  return (
+                    <span
+                      key={i}
+                      className="badge badge-pill text-primary border border-primary tag mx-1"
+                    >
+                      {item}
+                    </span>
+                  );
+                })}
+              </div>
+              <h4>{title}</h4>
+              <p>{description}</p>
+              <div className="user">
+                <div className="author-img mx-1">
+                  <Image src={Img} alt="Author" height={45} width={45} />
+                </div>
+                <div className="user-info">
+                  <h5>{author}</h5>
+                  <small>{date}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
