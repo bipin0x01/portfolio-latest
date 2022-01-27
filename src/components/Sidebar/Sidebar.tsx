7ca9c7f1431a7ce23eccd083e71c6e69a2cfeb02
Bipin Thapa <bt.kaji@gmail.com>
2022-01-27 22:44:52 +0545

    sidebar button styling fix

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -16,40 +16,48 @@ export default function Sidebar() {
           <div className="sidebar-buttons">
             <div className="sidebar-button">
               <Link href={sidebarLinks.facebook} passHref>
-                <button type="button" className="btn btn-primary">
-                  <h2>
-                    <FaFacebook />
-                  </h2>
-                </button>
+                <a target="_blank">
+                  <button type="button" className="btn btn-primary">
+                    <h2>
+                      <FaFacebook />
+                    </h2>
+                  </button>
+                </a>
               </Link>
             </div>
             <div className="sidebar-button">
               <Link href={sidebarLinks.linkedin} passHref>
-                <button type="button" className="btn btn-primary">
-                  <h2>
-                    <FaLinkedin />
-                  </h2>
-                </button>
+                <a target="_blank">
+                  <button type="button" className="btn btn-primary">
+                    <h2>
+                      <FaLinkedin />
+                    </h2>
+                  </button>
+                </a>
               </Link>
             </div>
             <div className="sidebar-button">
               <Link href={sidebarLinks.github} passHref>
-                <button type="button" className="btn btn-primary">
-                  <h2>
-                    <FaGithub />
-                  </h2>
-                </button>
+                <a target="_blank">
+                  <button type="button" className="btn btn-primary">
+                    <h2>
+                      <FaGithub />
+                    </h2>
+                  </button>
+                </a>
               </Link>
             </div>
             <div className="sidebar-button-cv sidebar-button">
               <Link href={sidebarLinks.resume} passHref>
-                <button type="button" className="btn btn-primary btn-lg">
-                  <FaDownload />
-                  <div className="download-cv">
-                    Download
-                    <br /> Resume
-                  </div>
-                </button>
+                <a target="_blank">
+                  <button type="button" className="btn btn-primary btn-lg">
+                    <FaDownload />
+                    <div className="download-cv">
+                      Download
+                      <br /> Resume
+                    </div>
+                  </button>
+                </a>
               </Link>
             </div>
           </div>
